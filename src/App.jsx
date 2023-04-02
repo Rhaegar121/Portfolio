3134a10596013269bad5e501cbd182b362d3be3a
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-03 00:02:31 +0630

    remove app.css and unnecessary css in index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import StarsCanvas from './components/Stars'
-import './App.css'
+// import './App.css'
 
 function App() {
 
   return (
     <BrowserRouter>
       <div className='relative z-0'>
+        <Navbar />
         <StarsCanvas />
+      <div className='relative z-0'>
+      </div>
         <Navbar />
       </div>
     </BrowserRouter>
