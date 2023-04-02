5a607f4d737243307b88f06686758a50f453fa2d
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-03 01:13:08 +0630

    reinstall tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import StarsCanvas from './components/Stars'
-// import './App.css'
+import StarsCanvas from './components/Stars';
 
 function App() {
 
   return (
     <BrowserRouter>
       <div className='relative z-0'>
-        <Navbar />
         <StarsCanvas />
-      <div className='relative z-0'>
-      </div>
         <Navbar />
       </div>
     </BrowserRouter>
