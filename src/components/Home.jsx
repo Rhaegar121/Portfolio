6f813abb41773b857a83446f81edbd969c8bf71c
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-08 14:59:08 +0630

    add hover effect to bottom

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       <p className={`${style.intro} text-cyan`}>Hi, I'm <span className={`${style.name} text-grey`}>Kaung Myat Kyaw</span></p>
       <p className={`${style.intro} text-cyan`}>But you can call me <span className={`${style.name} text-grey`}>Barry</span></p>
       <p className={`${style.desc} text-grey`}>I am a full-stack web developer</p>
-      <button className={`${style.btn} text-cyan`}>
+      <button className={`${style.btn} text-grey`}>
         Check out my work
         <MdOutlineArrowForwardIos 
         className='inline ml-2 text-lg'
