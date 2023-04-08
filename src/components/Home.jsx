3a3cbb5772d68ff099c2e89f570b22f78af24a15
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-08 14:27:34 +0630

    add arrow icon in bottom

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import style from './styles/home.module.css'
+import { MdOutlineArrowForwardIos } from 'react-icons/md';
 
 const Home = () => {
   return (
@@ -7,7 +8,12 @@ const Home = () => {
       <p className={`${style.intro} text-cyan`}>Hi, I'm <span className={`${style.name} text-grey`}>Kaung Myat Kyaw</span></p>
       <p className={`${style.intro} text-cyan`}>But you can call me <span className={`${style.name} text-grey`}>Barry</span></p>
       <p className={`${style.desc} text-grey`}>I am a full-stack web developer</p>
-      <button className={`${style.btn} text-cyan`}>Check out my work</button>
+      <button className={`${style.btn} text-cyan`}>
+        Check out my work
+        <MdOutlineArrowForwardIos 
+        className='inline ml-2 text-lg'
+        />
+      </button>
     </div>
   )
 }
