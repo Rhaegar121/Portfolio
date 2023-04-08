38515b82832c2f94cc61a7b5bc141b36158739ba
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-08 15:37:17 +0630

    change logo size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from '../assets/logo-no-background.png';
+import style from './styles/navbar.module.css';
 
 const Navbar = () => {
   return (
     <nav>
       <div>
-        <img src={logo} alt='logo' className='logo' />
+        <img src={logo} alt='logo' className={style.logo} />
       </div>
     </nav>
   )
