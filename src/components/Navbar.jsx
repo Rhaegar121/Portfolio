236b05ab28b29f655b9fd6a130bcec16954356ac
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-08 22:05:03 +0630

    add hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,11 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   const [active, setActive] = useState("");
   const [scrolled, setScrolled] = useState(false);
+  const [isOpen, setOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setOpen(!isOpen);
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -24,53 +29,46 @@ const Navbar = () => {
 
   return (
     <nav className={style.navbar}>
-      <Link
-        onClick={() => {
-          window.scrollTo(0, 0);
-        }}
-      >
+      <Link onClick={() => { window.scrollTo(0, 0); }} >
         <img src={logo} alt='logo' className={style.logo} />
       </Link>
       <ul className={style.navlink}>
         <li 
           key="about"
-          className={`${
-          active === "About" ? "text-cyan" : "text-grey"
-          } ${style.font}`}
+          className={`${ active === "About" ? "text-cyan" : "text-grey" } ${style.font}`}
           onClick={() => setActive("About")}
         >
           <a href={`#about`}>About</a>
         </li>
         <li 
           key="work"
-          className={`${
-          active === "Work" ? "text-cyan" : "text-grey"
-          } ${style.font}`}
+          className={`${ active === "Work" ? "text-cyan" : "text-grey" } ${style.font}`}
           onClick={() => setActive("Work")}
         >
-        <a href={`#work`}>Work</a>
+          <a href={`#work`}>Work</a>
         </li>
         <li 
           key="testimonials"
-          className={`${
-          active === "Testimonials" ? "text-cyan" : "text-grey"
-          } ${style.font}`}
+          className={`${ active === "Testimonials" ? "text-cyan" : "text-grey" } ${style.font}`}
           onClick={() => setActive("Testimonials")}
         >
-        <a href={`#testimonials`}>Testimonials</a>
+          <a href={`#testimonials`}>Testimonials</a>
         </li>
         <li 
           key="contact"
-          className={`${
-          active === "Contact" ? "text-cyan" : "text-grey"
-          } ${style.font}`}
+          className={`${ active === "Contact" ? "text-cyan" : "text-grey" } ${style.font}`}
           onClick={() => setActive("Contact")}
         >
-        <a href={`#contact`}>Contact</a>
+          <a href={`#contact`}>Contact</a>
         </li>
         <li className={`${style.resume} ${style.font}`}>
-        <a href={`#resume`}>Resume</a>
+          <a href={`#resume`}>Resume</a>
         </li>
+      </ul>
+      <ul className={style.hamburger} onClick={handleMenuClick}>
+        <li className={`${style.line} ${isOpen ? style.open : ""}`}></li>
+        <li className={`${style.line} ${isOpen ? style.open : ""}`}></li>
+        <li className={`${style.line} ${isOpen ? style.open : ""}`}></li>
       </ul>
     </nav>
   )
