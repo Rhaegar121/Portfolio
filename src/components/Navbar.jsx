69998af4b35b0981ce936f1684b60401f4c364a3
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-08 17:12:00 +0630

    add active effect in navlink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import logo from '../assets/logo-no-background.png';
 import style from './styles/navbar.module.css';
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <nav className={style.navbar}>
       <Link
@@ -14,19 +32,49 @@ const Navbar = () => {
         <img src={logo} alt='logo' className={style.logo} />
       </Link>
       <ul className={style.navlink}>
-        <li className={style.font}>
+        <li 
+          key="about"
+          className={`${
+          active === "About" ? "text-cyan" : "text-grey"
+          } ${style.font}`}
+          onClick={() => setActive("About")}
+        >
           <a href={`#about`}>About</a>
         </li>
-        <li className={style.font}>
+        <li 
+          key="work"
+          className={`${
+          active === "Work" ? "text-cyan" : "text-grey"
+          } ${style.font}`}
+          onClick={() => setActive("Work")}
+        >
         <a href={`#work`}>Work</a>
         </li>
-        <li className={style.font}>
+        <li 
+          key="testimonials"
+          className={`${
+          active === "Testimonials" ? "text-cyan" : "text-grey"
+          } ${style.font}`}
+          onClick={() => setActive("Testimonials")}
+        >
         <a href={`#testimonials`}>Testimonials</a>
         </li>
-        <li className={style.font}>
+        <li 
+          key="contact"
+          className={`${
+          active === "Contact" ? "text-cyan" : "text-grey"
+          } ${style.font}`}
+          onClick={() => setActive("Contact")}
+        >
         <a href={`#contact`}>Contact</a>
         </li>
-        <li className={style.font}>
+        <li 
+          key="resume"
+          className={`${
+          active === "Resume" ? "text-cyan" : "text-grey"
+          } ${style.font}`}
+          onClick={() => setActive("Resume")}
+        >
         <a href={`#resume`}>Resume</a>
         </li>
       </ul>
