f8f2b1fb442be2fa0d558039c55cd5cb5393bd62
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-09 20:30:46 +0630

    add about.module.css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { SectionWrapper } from '../hoc';
 
 const About = () => {
   return (
-    <div className='h-screen'>About</div>
+    <div className='h-screen'>
+      <h1>About Me</h1>
+      <p>My name is Kaung Myat Kyaw (Barry), and I'm currently completing a full-stack development course at Microverse, a remote software development school that uses pair programming and real-world projects to teach development.</p>
+      <p>I've spent the last several months developing real-world projects from scratch in a remote environment. I worked as a general secretary and a social welfare officer at the Students' Union of my University for two years. It is not the same type of work as development, but it taught me some of the skills I now use every day as a developer: problem-solving, working under pressure, effective communication, and more.</p>
+      <p></p>
+    </div>
   )
 }
 
-export default About
+export default SectionWrapper(About, "about");
