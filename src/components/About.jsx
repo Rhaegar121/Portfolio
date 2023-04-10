4284c1832f174881ec7589b7f35b4a352824fa5e
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-11 01:14:11 +0630

    change name color in the about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
   return (
     <div>
       <h1 className={style.title}>About Me</h1>
-      <p className={style.para}>My name is Kaung Myat Kyaw (Barry), and I've spent the last several months developing real-world projects from scratch in a remote environment. I worked as a general secretary and a social welfare officer at the Students' Union of my University for two years. It is not the same type of work as development, but it taught me some of the skills I now use every day as a developer: problem-solving, working under pressure, effective communication, and more.</p>
+      <p className={style.para}>My name is <span className={style.name}>Kaung Myat Kyaw (Barry)</span>, and I've spent the last several months developing real-world projects from scratch in a remote environment. I worked as a general secretary and a social welfare officer at the Students' Union of my University for two years. It is not the same type of work as development, but it taught me some of the skills I now use every day as a developer: problem-solving, working under pressure, effective communication, and more.</p>
       <p className={style.para}>Here are a few technologies I`ve been working with recently:</p>
       <div className={style.ball_container}>
       {technologies.map((technology) => (
