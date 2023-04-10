2842a93dbe123421f2378827eb0212d3080d045c
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-11 00:57:00 +0630

    change the style of ball container

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
       <h1 className={style.title}>About Me</h1>
       <p className={style.para}>My name is Kaung Myat Kyaw (Barry), and I've spent the last several months developing real-world projects from scratch in a remote environment. I worked as a general secretary and a social welfare officer at the Students' Union of my University for two years. It is not the same type of work as development, but it taught me some of the skills I now use every day as a developer: problem-solving, working under pressure, effective communication, and more.</p>
       <p className={style.para}>Here are a few technologies I`ve been working with recently:</p>
-      <div className='flex flex-row flex-wrap justify-center gap-10'>
+      <div className={style.ball_container}>
       {technologies.map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
           <BallCanvas icon={technology.icon} />
