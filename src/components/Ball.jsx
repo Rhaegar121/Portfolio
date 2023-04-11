9adc0138b676101cc35709fc419ef7dfa7eef73f
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-11 12:24:15 +0630

    remove blur effect and add background color

--- src/components/Ball.jsx
+++ src/components/Ball.jsx
@@ -12,7 +12,7 @@ const Ball = ({imgUrl, isMobile}) => {
   const [decal] = useTexture([imgUrl]);
 
   return (
-    <Float speed={0.2} rotationIntensity={0.5} floatIntensity={1.5}>
+    <Float speed={0.75} rotationIntensity={0.5} floatIntensity={1.5}>
       <ambientLight intensity={0.6} />
       <mesh castShadow receiveShadow scale={isMobile ? 2.1 : 2.5} >
         <sphereGeometry args={[1, 32.8, 32.8]} />
