c20b618cfd87c14bbb9b69de24ddfbd897b0dcdb
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-11 22:05:27 +0630

    change card overlay style in work component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import style from './styles/work.module.css'
 import { SectionWrapper } from '../hoc'
+import { projects } from './constants'
 
 const Work = () => {
   return (
-    <div className='h-screen'>
+    <div>
       <h1 className={style.title}>My Recent Works</h1>
+      <div className={style.project_container}>
+        {projects.map((project) => (
+          <div className={style.card}>
+            <img className={style.img} src={project.img} alt="project sample" />
+            <div className={style.card_overlay}>
+              <div className={style.card_text}>
+              <div className={style.text}>
+                <p>{project.name}</p>
+                <p>
+                  <span>{project.tech[0]}</span>
+                  <span>{project.tech[1]}</span>
+                  <span>{project.tech[2]}</span>
+                </p>
+              </div>
+              <button className={style.btn}>Learn More</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
