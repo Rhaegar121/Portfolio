0fb30653029e9a2f18517e3cd054e245482a29af
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-11 23:09:18 +0630

    add motion to the project card

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,17 +2,15 @@ import React from 'react'
 import style from './styles/work.module.css'
 import { SectionWrapper } from '../hoc'
 import { projects } from './constants'
+import { motion } from "framer-motion";
+import { fadeIn } from '../utils/motion';
 
-const Work = () => {
+const ProjectCard = ({ project, index }) => {
   return (
-    <div>
-      <h1 className={style.title}>My Recent Works</h1>
-      <div className={style.project_container}>
-        {projects.map((project) => (
-          <div className={style.card}>
-            <img className={style.img} src={project.img} alt="project sample" />
-            <div className={style.card_overlay}>
-              <div className={style.card_text}>
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.5)} className={style.card}>
+      <img className={style.img} src={project.img} alt="project sample" />
+          <div className={style.card_overlay}>
+            <div className={style.card_text}>
               <div className={style.text}>
                 <h2 className={style.name}>{project.name}</h2>
                 <p className={style.tech}>
@@ -21,10 +19,20 @@ const Work = () => {
                   <span className={style.tech_list}>{`#${project.tech[2]}`}</span>
                 </p>
               </div>
-              <button className={style.btn}>Learn More</button>
-              </div>
-            </div>
+            <button className={style.btn}>Learn More</button>
           </div>
+      </div>
+    </motion.div>
+  )
+}
+
+const Work = () => {
+  return (
+    <div>
+      <h1 className={style.title}>My Recent Works</h1>
+      <div className={style.project_container}>
+        {projects.map((project, index) => (
+          <ProjectCard key={project.name} index={index} project={project} />
         ))}
       </div>
     </div>
