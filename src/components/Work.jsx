db94cde47ccd6aa3751a7f6eb9a4c65c74a0c5d0
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-11 21:09:22 +0630

    change title style in the work component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import style from './styles/work.module.css'
+import { SectionWrapper } from '../hoc'
 
 const Work = () => {
   return (
-    <div className='h-screen'>Work</div>
+    <div className='h-screen'>
+      <h1 className={style.title}>My Recent Works</h1>
+    </div>
   )
 }
 
-export default Work
+export default SectionWrapper(Work, "work");
