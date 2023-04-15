67bf7afebf2832c2b01033298e6f3db1632517ff
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-16 00:32:20 +0630

    add motion effect in about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,16 +4,16 @@ import style from './styles/about.module.css';
 import BallCanvas from './Ball';
 import { technologies } from '../constants';
 import { motion } from "framer-motion";
-import { textVariant, fadeIn } from '../utils/motion';
+import { textVariant, fadeIn, slideIn } from '../utils/motion';
 import mypicture from '../assets/mypicture.jpg';
-// variants={fadeIn("", "", 0.15, 1)}
+
 const About = () => {
   return (
     <div>
       <motion.h1 variants={textVariant()} className={style.title}>About Me</motion.h1>
       <div className={style.content}>
-        <motion.p className={style.para}>My name is <span className={style.name}>Kaung Myat Kyaw (Barry)</span>, and I've spent the last several months developing real-world projects from scratch in a remote environment. I worked as a general secretary and a social welfare officer at the Students' Union of my University for two years. It is not the same type of work as development, but it taught me some of the skills I now use every day as a developer: problem-solving, working under pressure, effective communication, and more.</motion.p>
-        <motion.div className={style.img_container}>
+        <motion.p variants={slideIn("left", "", 0, 1)} className={style.para}>My name is <span className={style.name}>Kaung Myat Kyaw (Barry)</span>, and I've spent the last several months developing real-world projects from scratch in a remote environment. I worked as a general secretary and a social welfare officer at the Students' Union of my University for two years. It is not the same type of work as development, but it taught me some of the skills I now use every day as a developer: problem-solving, working under pressure, effective communication, and more.</motion.p>
+        <motion.div variants={slideIn("right", "", 0, 1)} className={style.img_container}>
           <div className={style.overlay}></div>
           <img className={style.img} src={mypicture} alt="my picture" />
         </motion.div>
