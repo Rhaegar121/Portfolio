7a5e89a00a9a9ee32e895b01c5de32062682d36e
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-15 19:00:50 +0630

    add linkedIn icon in testimonial component

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { motion } from "framer-motion";
 import { fadeIn, textVariant } from "../utils/motion";
 import { SectionWrapper } from '../hoc';
-import { BsPersonCircle } from 'react-icons/bs';
+import { FaLinkedinIn } from 'react-icons/fa';
 import { FaQuoteLeft } from 'react-icons/fa';
 import style from './styles/testimonial.module.css';
 
@@ -20,27 +20,27 @@ const Testimonial = () => {
           <p className={style.para}>I highly recommend Barry for any software development role. During our recent collaboration, he consistently demonstrated exceptional coding skills and a strong work ethic. He is a pleasure to work with and his attention to detail and commitment to quality make him an excellent candidate for any software development team.</p>
           <div className={style.label}>
             <h2 className={style.name}>Wai Yan Phyo</h2>
-            <BsPersonCircle className={style.icon} />
+            <a><FaLinkedinIn className={style.icon} /></a>
           </div>
           </div>
         </div>
         <div className={style.card_shadow}>
           <div className={style.card}>
           <FaQuoteLeft className={style.openquote} />
-          <p className={style.para}>I highly recommend Barry for any software development role. During our recent collaboration, he consistently demonstrated exceptional coding skills and a strong work ethic. He is a pleasure to work with and his attention to detail and commitment to quality make him an excellent candidate for any software development team.</p>
+          <p className={style.para}>Barry is one of the brightest software developers I've ever met. I worked alongside him while building a single-page Web Application for movie display, and in that time he proved his commitment and determination over and over again. Barry is always challenging himself--he's the type of person who reads documentation for fun, and his attention to detail is evident in all of the projects I've worked with him on. He's also genuinely a kind, encouraging person, and kept me from falling into bug blues multiple times. I can recommend him without reservation.</p>
           <div className={style.label}>
-            <h2 className={style.name}>Wai Yan Phyo</h2>
-            <BsPersonCircle className={style.icon} />
+            <h2 className={style.name}>Nwadinigwe Victor</h2>
+            <a><FaLinkedinIn className={style.icon} /></a>
           </div>
           </div>
         </div>
         <div className={style.card_shadow}>
           <div className={style.card}>
           <FaQuoteLeft className={style.openquote} />
-          <p className={style.para}>I highly recommend Barry for any software development role. During our recent collaboration, he consistently demonstrated exceptional coding skills and a strong work ethic. He is a pleasure to work with and his attention to detail and commitment to quality make him an excellent candidate for any software development team.</p>
+          <p className={style.para}>Barry was my first coding partner. I remember how glad I was passing a week and spent creating a project and solving lots of programming challenges together. I really am comfortable with collaborating and coding with Barry. His ingenious ideas give me a lot of ideas solving a problem multiple ways.</p>
           <div className={style.label}>
-            <h2 className={style.name}>Wai Yan Phyo</h2>
-            <BsPersonCircle className={style.icon} />
+            <h2 className={style.name}>Ahmed Hasan Rony</h2>
+            <a><FaLinkedinIn className={style.icon} /></a>
           </div>
           </div>
         </div>
