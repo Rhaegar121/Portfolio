66c0da8bbbd9332806044516b44e9a54a8c89c9b
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-15 22:24:07 +0630

    change stars color in star component

--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -17,7 +17,7 @@ const Stars = (props) => {
       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
-          color='#ec9292'
+          color='#ffffff'
           size={0.002}
           sizeAttenuation={true}
           depthWrite={false}
