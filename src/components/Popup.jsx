a49790dbdfb8ccbad6e26340fc48615dc33362fd
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-15 14:19:09 +0630

    add close icon in popup component

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import style from "./styles/popup.module.css";
+import { AiOutlineClose } from 'react-icons/ai';
 
 const Popup = ({ handleClose, project }) => {
   return (
     <div className={style.overlay}>
       <div className={style.container}>
-        <button className={style.close_btn} onClick={handleClose}>
-          Close
-        </button>
+        <AiOutlineClose className={style.close_btn} onClick={handleClose} />
         <img className={style.img} src={project.img} alt="project sample" />
         <div className={style.content}>
             <h2 className={style.name}>{project.name}</h2>
