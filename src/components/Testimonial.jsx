80b02848e307a2d74af93c6f2689b8777dda5d68
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-15 19:36:01 +0630

    add media query 768 and 1200 for testimonial component

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -18,7 +18,7 @@ const Testimonial = () => {
           <div className={style.card}>
           <FaQuoteLeft className={style.openquote} />
           <p className={style.para}>I highly recommend Barry for any software development role. During our recent collaboration, he consistently demonstrated exceptional coding skills and a strong work ethic. He is a pleasure to work with and his attention to detail and commitment to quality make him an excellent candidate for any software development team.</p>
-          <div className={style.label}>
+          <div className={`${style.label} ${style.firstlabel}`}>
             <h2 className={style.name}>Wai Yan Phyo</h2>
             <a><FaLinkedinIn className={style.icon} /></a>
           </div>
@@ -27,7 +27,7 @@ const Testimonial = () => {
         <div className={style.card_shadow}>
           <div className={style.card}>
           <FaQuoteLeft className={style.openquote} />
-          <p className={style.para}>Barry is one of the brightest software developers I've ever met. I worked alongside him while building a single-page Web Application for movie display, and in that time he proved his commitment and determination over and over again. Barry is always challenging himself--he's the type of person who reads documentation for fun, and his attention to detail is evident in all of the projects I've worked with him on. He's also genuinely a kind, encouraging person, and kept me from falling into bug blues multiple times. I can recommend him without reservation.</p>
+          <p className={style.para}>Barry is one of the brightest software developers I've ever met. I worked alongside him while building a single-page Web Application for movie display, and in that time he proved his commitment and determination over and over again. Barry is always challenging himself-- he's the type of person who reads documentation for fun, and his attention to detail is evident in all of the projects I've worked with him on.</p>
           <div className={style.label}>
             <h2 className={style.name}>Nwadinigwe Victor</h2>
             <a><FaLinkedinIn className={style.icon} /></a>
@@ -38,7 +38,7 @@ const Testimonial = () => {
           <div className={style.card}>
           <FaQuoteLeft className={style.openquote} />
           <p className={style.para}>Barry was my first coding partner. I remember how glad I was passing a week and spent creating a project and solving lots of programming challenges together. I really am comfortable with collaborating and coding with Barry. His ingenious ideas give me a lot of ideas solving a problem multiple ways.</p>
-          <div className={style.label}>
+          <div className={`${style.label} ${style.lastlabel}`}>
             <h2 className={style.name}>Ahmed Hasan Rony</h2>
             <a><FaLinkedinIn className={style.icon} /></a>
           </div>
