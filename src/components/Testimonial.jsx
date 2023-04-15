8452600a2a969b933ce2bc63765008077008a3c5
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-15 21:56:11 +0630

    add motion to testimonial component

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -12,9 +12,9 @@ const Testimonial = () => {
       <motion.h1 variants={textVariant()} className={style.title}>
         Testimonials
       </motion.h1>
-      <p className={style.subtitle}>What my coding partners say about me -</p>
+      <motion.p variants={fadeIn("", "", 0.15, 1)} className={style.subtitle}>What my coding partners say about me -</motion.p>
       <div className={style.card_container}>
-        <div className={style.card_shadow}>
+        <motion.div variants={fadeIn("right", "", 0.5, 0.5)} className={style.card_shadow}>
           <div className={style.card}>
           <FaQuoteLeft className={style.openquote} />
           <p className={style.para}>I highly recommend Barry for any software development role. During our recent collaboration, he consistently demonstrated exceptional coding skills and a strong work ethic. He is a pleasure to work with and his attention to detail and commitment to quality make him an excellent candidate for any software development team.</p>
@@ -23,8 +23,8 @@ const Testimonial = () => {
             <a><FaLinkedinIn className={style.icon} /></a>
           </div>
           </div>
-        </div>
-        <div className={style.card_shadow}>
+        </motion.div>
+        <motion.div variants={fadeIn("right", "", 1, 0.5)} className={style.card_shadow}>
           <div className={style.card}>
           <FaQuoteLeft className={style.openquote} />
           <p className={style.para}>Barry is one of the brightest software developers I've ever met. I worked alongside him while building a single-page Web Application for movie display, and in that time he proved his commitment and determination over and over again. Barry is always challenging himself-- he's the type of person who reads documentation for fun, and his attention to detail is evident in all of the projects I've worked with him on.</p>
@@ -33,8 +33,8 @@ const Testimonial = () => {
             <a><FaLinkedinIn className={style.icon} /></a>
           </div>
           </div>
-        </div>
-        <div className={style.card_shadow}>
+        </motion.div>
+        <motion.div variants={fadeIn("right", "", 1.5, 0.5)} className={style.card_shadow}>
           <div className={style.card}>
           <FaQuoteLeft className={style.openquote} />
           <p className={style.para}>Barry was my first coding partner. I remember how glad I was passing a week and spent creating a project and solving lots of programming challenges together. I really am comfortable with collaborating and coding with Barry. His ingenious ideas give me a lot of ideas solving a problem multiple ways.</p>
@@ -43,7 +43,7 @@ const Testimonial = () => {
             <a><FaLinkedinIn className={style.icon} /></a>
           </div>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   )
