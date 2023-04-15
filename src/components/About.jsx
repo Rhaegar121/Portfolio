084133f2701b858f1522654a97195b09c70942bd
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-15 23:42:01 +0630

    add hover effect to img in about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,13 +5,20 @@ import BallCanvas from './Ball';
 import { technologies } from '../constants';
 import { motion } from "framer-motion";
 import { textVariant, fadeIn } from '../utils/motion';
-
+import mypicture from '../assets/mypicture.jpg';
+// variants={fadeIn("", "", 0.15, 1)}
 const About = () => {
   return (
     <div>
       <motion.h1 variants={textVariant()} className={style.title}>About Me</motion.h1>
-      <motion.p variants={fadeIn("", "", 0.15, 1)} className={style.para}>My name is <span className={style.name}>Kaung Myat Kyaw (Barry)</span>, and I've spent the last several months developing real-world projects from scratch in a remote environment. I worked as a general secretary and a social welfare officer at the Students' Union of my University for two years. It is not the same type of work as development, but it taught me some of the skills I now use every day as a developer: problem-solving, working under pressure, effective communication, and more.</motion.p>
-      <motion.p variants={fadeIn("", "", 0.15, 1)} className={style.para}>Here are a few technologies I`ve been working with recently:</motion.p>
+      <div className={style.content}>
+        <motion.p className={style.para}>My name is <span className={style.name}>Kaung Myat Kyaw (Barry)</span>, and I've spent the last several months developing real-world projects from scratch in a remote environment. I worked as a general secretary and a social welfare officer at the Students' Union of my University for two years. It is not the same type of work as development, but it taught me some of the skills I now use every day as a developer: problem-solving, working under pressure, effective communication, and more.</motion.p>
+        <motion.div className={style.img_container}>
+          <div className={style.overlay}></div>
+          <img className={style.img} src={mypicture} alt="my picture" />
+        </motion.div>
+      </div>
+      <motion.p variants={fadeIn("", "", 0.15, 1)} className={`${style.subtitle} ${style.para}`}>Here are a few technologies I`ve been working with recently:</motion.p>
       <motion.div variants={fadeIn("", "", 0.5, 1)} className={style.ball_container}>
       {technologies.map((technology) => (
         <div className={style.ball} key={technology.name}>
