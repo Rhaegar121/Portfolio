073f13059c67fdc7e1549b262f74d231b9c1cdeb
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-15 18:05:41 +0630

    add frame layout in testimonial component

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { motion } from "framer-motion";
 import { fadeIn, textVariant } from "../utils/motion";
 import { SectionWrapper } from '../hoc';
+import { BsPersonCircle } from 'react-icons/bs';
 // import style from './styles/testimonial.module.css';
 
 const Testimonial = () => {
@@ -10,6 +11,17 @@ const Testimonial = () => {
       <motion.h1 variants={textVariant()}>
         Testimonials
       </motion.h1>
+      <p>What other people say about me</p>
+      <div>
+        <div>
+          <span></span>
+          <p></p>
+          <div>
+            <h2></h2>
+            <BsPersonCircle />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
