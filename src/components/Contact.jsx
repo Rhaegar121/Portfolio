4b5feb03f7a9e580706cf17adf195e12d265b7c1
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-16 15:32:47 +0630

    change title font size in all component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { SectionWrapper } from '../hoc';
 import { motion } from "framer-motion";
 import { fadeIn, textVariant } from "../utils/motion";
+import style from './styles/contact.module.css';
 
 const Contact = () => {
   return (
