cd0088d8db5d82e9df2962ebd177f54f55b7f9aa
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-16 15:43:15 +0630

    add subtitle in contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,10 +6,38 @@ import style from './styles/contact.module.css';
 
 const Contact = () => {
   return (
-    <div className='h-screen'>
+    <div>
       <motion.h1 variants={textVariant()} className={style.title}>
         Contact
       </motion.h1>
+      <motion.p variants={fadeIn("", "", 0.15, 1)} className={style.subtitle}>
+        I'm always excited to hear about new opportunities and collaborations. Don't hesitate to reach out and let's make something great.
+      </motion.p>
+      <motion.form action="https://formspree.io/f/mgeqgkdd" method="post">
+          <input
+            type="text"
+            placeholder="Full name"
+            required
+            maxlength="30"
+            name="name"
+          />
+          <input
+            type="email"
+            placeholder="Email address"
+            required
+            name="email"
+          />
+          <span></span>
+          <textarea
+            cols="30"
+            rows="10"
+            placeholder="Say something"
+            required
+            maxlength="500"
+            name="message"
+          ></textarea>
+          <button type="submit">Get in touch</button>
+      </motion.form>
     </div>
   )
 }
