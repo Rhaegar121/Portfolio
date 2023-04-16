94ab9bf036f3ccc92c94ea86e3868283cf2f63b2
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-16 22:36:15 +0630

    add icon in the footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import style from './styles/footer.module.css';
+import { FiFacebook } from 'react-icons/fi';
+// import { FiLinkedin } from 'react-icons/fi';
+// import { FiInstagram } from 'react-icons/fi';
+// import { FiTwitter } from 'react-icons/fi';
+// import { FiGithub } from 'react-icons/fi';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={style.icon_container}>
+        <div className={style.overlay}><FiFacebook className={style.icon} /></div>
+        <div className={style.overlay}><FiFacebook className={style.icon} /></div>
+        <div className={style.overlay}><FiFacebook className={style.icon} /></div>
+        <div className={style.overlay}><FiFacebook className={style.icon} /></div>
+        <div className={style.overlay}><FiFacebook className={style.icon} /></div>
+    </div>
   )
 }
 
