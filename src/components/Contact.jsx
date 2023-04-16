870970076bf4aea2f626686a2e51bd59946b266a
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-16 15:28:08 +0630

    add title and sectionwrapper in contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { SectionWrapper } from '../hoc';
+import { motion } from "framer-motion";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const Contact = () => {
   return (
-    <div className='h-screen'>Contact</div>
+    <div className='h-screen'>
+      <motion.h1 variants={textVariant()} className={style.title}>
+        Contact
+      </motion.h1>
+    </div>
   )
 }
 
-export default Contact
+export default SectionWrapper(Contact, "contact");
