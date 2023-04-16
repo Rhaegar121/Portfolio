6e09211376b5a2513526d1995fe8700d59b1a5bb
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-16 17:43:04 +0630

    change para section in about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,11 @@ const About = () => {
     <div>
       <motion.h1 variants={textVariant()} className={style.title}>About Me</motion.h1>
       <div className={style.content}>
-        <motion.p variants={slideIn("left", "", 0, 1)} className={style.para}>My name is <span className={style.name}>Kaung Myat Kyaw (Barry)</span>, and I've spent the last several months developing real-world projects from scratch in a remote environment. I worked as a general secretary and a social welfare officer at the Students' Union of my University for two years. It is not the same type of work as development, but it taught me some of the skills I now use every day as a developer: problem-solving, working under pressure, effective communication, and more.</motion.p>
+        <motion.div variants={slideIn("left", "", 0, 1)} className={style.para}>
+          <p className={style.text}>My name is <a href='https://www.linkedin.com/in/kaungmyatkyaw/' className={style.link}>Kaung Myat Kyaw (Barry)</a>, and I've spent the last several months developing real-world projects from scratch in a remote environment. I love math, programming, movies, and all kinds of music and according to GitHub, I am now <a href='https://commits.top/myanmar_public.html' className={style.link}>Top 7</a> contributor in my country.</p>
+          <p className={style.text}>I also worked as a general secretary and a social welfare officer at the Students' Union of my University for two years. It is not the same type of work as development, but it taught me some of the skills I now use every day as a developer: problem-solving, working under pressure, effective communication, and more.</p>
+          <p className={style.text}>I can help you build a product, feature, or website. Look through some of my work and experience! If you like what you see and have a project you need to be coded for, don't hesitate to contact me.</p>
+        </motion.div>
         <motion.div variants={slideIn("right", "", 0, 1)} className={style.img_container}>
           <div className={style.overlay}></div>
           <img className={style.img} src={mypicture} alt="my picture" />
