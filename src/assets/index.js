3e537070db1b8d312e9cd21a2d94b9b4da27db47
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-17 21:33:55 +0630

    import all carousel img in index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,8 +8,24 @@ import tictactoe from './projects/tictactoe.jpg'
 import mathmagician from './projects/mathmagician.png'
 import metricwebapp from './projects/metricwebapp.png'
 import spacetravelers from './projects/spacetravelers.png'
+
+// carousel img
 import awesomebook1 from './carousels/awesome book screenshot 1.jpg';
 import awesomebook2 from './carousels/awesome book screenshot 2.jpg';
 import awesomebook3 from './carousels/awesome book screenshot 3.jpg';
+import guesstheword1 from './carousels/guess the word screenshot 1.jpg';
+import guesstheword2 from './carousels/guess the word screenshot 2.jpg';
+import guesstheword3 from './carousels/guess the word screenshot 3.jpg';
+import spacetravelers1 from './carousels/space travelers screenshot 1.png';
+import spacetravelers2 from './carousels/space travelers screenshot 2.png';
+import spacetravelers3 from './carousels/space travelers screenshot 3.png';
+import tictactoe1 from './carousels/tic tac toe screenshot 1.jpg';
+import tictactoe2 from './carousels/tic tac toe screenshot 2.jpg';
+import tictactoe3 from './carousels/tic tac toe screenshot 3.jpg';
+import tvshow1 from './carousels/tvshow screenshot 1.jpg';
+import tvshow2 from './carousels/tvshow screenshot 2.jpg';
+import tvshow3 from './carousels/tvshow screenshot 3.jpg';
+import wdetsummit1 from './carousels/wdet summit screenshot 1.png';
+import wdetsummit2 from './carousels/wdet summit screenshot 2.png';
 
-export { logo, awesomebook , tvshowbox, todolist, wdetsummit, guesstheword, tictactoe, mathmagician, metricwebapp, spacetravelers, awesomebook1, awesomebook2, awesomebook3 };
+export { logo, awesomebook , tvshowbox, todolist, wdetsummit, guesstheword, tictactoe, mathmagician, metricwebapp, spacetravelers, awesomebook1, awesomebook2, awesomebook3, guesstheword1, guesstheword2, guesstheword3, spacetravelers1, spacetravelers2, spacetravelers3, tictactoe1, tictactoe2, tictactoe3, tvshow1, tvshow2, tvshow3, wdetsummit1, wdetsummit2 };
