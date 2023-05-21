ed52fae814bf02d98751f923b407ea037b0389f8
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-05-21 19:41:01 +0630

    change testimonial background color

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
         <Navbar />
         <About />
         <Work />
-        <div className='relative z-0 bg-light_grey mt-24 pb-12'><Testimonial /></div>
+        <div className='relative z-0 grey-gradient mt-24 pb-12'><Testimonial /></div>
         <div className="contact"><Contact /></div>
         <div className='bg-dark_blue relative'><Footer /></div>
       </div>
