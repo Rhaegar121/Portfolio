7ca1a12c472036e359adc3052df33b66c804d44f
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-06-18 22:42:46 +0630

    add pagination to the techstack component

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,26 +1,47 @@
 import React, { useState } from 'react';
 import { technologies } from '../constants';
+import { motion } from "framer-motion";
 import { fadeIn } from '../utils/motion';
 import style from './styles/techstack.module.css';
 
 const TechStack = () => {
-  const [stack, setStack] = useState('languages');
+  const [stack, setStack] = useState('all');
 
   const handlePageChange = (stack) => {
     setStack(stack);
   };
 
-  const stacks = ['languages', 'frameworks', 'tools'];
+  const stacks = ['all', 'languages', 'frameworks', 'tools'];
+  const filteredTechnologies = technologies.filter((technology) => technology.stack === stack);
 
   return (
     <div>
       <motion.p variants={fadeIn("", "", 0.15, 1)} className={style.title}>Here are a few technologies I`ve been working with recently:</motion.p>
+      <motion.div variants={fadeIn("", "", 0.2, 1)} className={style.btn_container}>
+        {stacks.map((stack) => (
+          <button
+            key={stack}
+            className={stack === stack ? style.btn_active : style.btn}
+            onClick={() => handlePageChange(stack)}
+          >
+            {stack}
+          </button>
+        ))}
+      </motion.div>
       <motion.div variants={fadeIn("", "", 0.25, 1)} className={style.tech_container}>
-        {technologies.map((technology) => (
-          <div key={technology.name} className={style.tech} >
-            <img className={style.tech_img} src={technology.icon} alt={technology.name} />
-          </div>
-          ))}
+        {stack === 'all' ? (
+          technologies.map((technology) => (
+            <div key={technology.name} className={style.tech} >
+              <img className={style.tech_img} src={technology.icon} alt={technology.name} />
+            </div>
+          ))
+        ) : (
+          filteredTechnologies.map((technology) => (
+            <div key={technology.name} className={style.tech} >
+              <img className={style.tech_img} src={technology.icon} alt={technology.name} />
+            </div>
+          ))
+        )}
       </motion.div>
     </div>
   )
