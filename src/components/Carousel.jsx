e827cb148cedcec6ecef80e8a3718515b7d8cf05
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-07-04 22:55:01 +0630

    update carousel arrow style and img size

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,30 +2,30 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import style from './styles/carousel.module.css';
+// import style from './styles/carousel.module.css';
+import '../index.css';
 
 const Carousel = ({ carousel }) => {
   const settings = {
     dots: true,
-    appendDots: (dots) => (
-      <div>
-        <ul>{dots}</ul>
-      </div>
-    ),
+    // appendDots: (dots) => (
+    //   <div>
+    //     <ul>{dots}</ul>
+    //   </div>
+    // ),
     infinite: true,
-    arrows: false,
-    speed: 500,
-    autoplay: true,
-    autoplaySpeed: 3000,
+    // arrows: false,
+    // speed: 500,
+    // autoplay: true,
+    // autoplaySpeed: 3000,
     slidesToShow: 1,
     slidesToScroll: 1,
-    className: style.slider,
   };
 
   return (
     <Slider {...settings}>
         {carousel.map((img) => (
-            <div className={style.img}>
+            <div className="carousel-img">
                 <img src={img} alt="project sample img" />
             </div>
         ))}   
