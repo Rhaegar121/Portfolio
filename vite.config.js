af5cac7db801fb75b5b4864365749e632e9284e5
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-07-04 23:30:58 +0630

    update order for slick-carousel css files

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createVuePlugin } from 'vite-plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [react(), createVuePlugin()],
+  css: {
+    order: {
+      after: ['slick-carousel/slick/slick.css', 'slick-carousel/slick/slick-theme.css'],
+    },
+  },
   build: {
     rollupOptions: {
-        output:{
-            manualChunks(id) {
-                if (id.includes('node_modules')) {
-                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                }
-            }
-        }
-    }
-  }
-})
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+  },
+});
