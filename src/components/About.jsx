f659d16cc6ab9b36736f98d9bdd9cbe9112ff439
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-07-07 22:20:07 +0630

    update about me text in about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,9 +12,9 @@ const About = () => {
       <motion.h1 variants={textVariant()} className={style.title}>About Me</motion.h1>
       <div className={style.content}>
         <motion.div variants={slideIn("left", "", 0, 1)} className={style.para}>
-          <p className={style.text}>My name is <a href='https://www.linkedin.com/in/kaungmyatkyaw/' target='_blank' className={style.link}>Kaung Myat Kyaw (Barry)</a>, and I've spent the last several months developing real-world projects from scratch in a remote environment. I love Math, Programming, Movies, and all kinds of Music.</p>
+          <p className={style.text}>My name is <a href='https://www.linkedin.com/in/kaungmyatkyaw/' target='_blank' className={style.link}>Kaung Myat Kyaw (Barry)</a> and I am a passionate software engineer for delivering state-of-the-art software solutions in React and Ruby on Rails. I've spent the last several months developing real-world projects from scratch in a remote environment. I love Math, Coding, Movies, and all kinds of Music.</p>
           <p className={style.text}>I also worked as a general secretary and a social welfare officer at the Students' Union of my University for two years. It is not the same type of work as development, but it taught me some of the skills I now use every day as a developer: problem-solving, working under pressure, effective communication, and more.</p>
-          <p className={style.text}>I can help you build a product, feature, or website. Look through some of my work and experience! If you like what you see and have a project you need to be coded for, don't hesitate to contact me.</p>
+          <p className={style.text}>I can help you build a product, feature, or website. Look through some of my work and experience! If you like what you see and have a project you need to be coded for, don't hesitate to send a message to me.</p>
         </motion.div>
         <motion.div variants={slideIn("right", "", 0, 1)} className={style.img_container}>
           <div className={style.overlay}></div>
