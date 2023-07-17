3f670c45d3c9354be6045a346aabc7c7a9f4d39a
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-07-18 01:29:07 +0630

    update introduction content in about compoennt

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,10 +16,11 @@ const About = () => (
           {' '}
           <a href="https://www.linkedin.com/in/kaungmyatkyaw/" target="_blank" className={style.link} rel="noreferrer">Kaung Myat Kyaw (Barry)</a>
           {' '}
-          and I am a passionate software engineer for delivering state-of-the-art software solutions in React and Ruby on Rails. I&apos;ve spent the last several months developing real-world projects from scratch in a remote environment. I love Math, Coding, Movies, and all kinds of Music.
+          and I am a passionate software engineer specializing in delivering state-of-the-art software solutions in React and Ruby on Rails. I love Math, Coding, Movies, and all kinds of Music.
         </p>
-        <p className={style.text}>I also worked as a general secretary and a social welfare officer at the Students&apos; Union of my University for two years. It is not the same type of work as development, but it taught me some of the skills I now use every day as a developer: problem-solving, working under pressure, effective communication, and more.</p>
-        <p className={style.text}>I can help you build a product, feature, or website. Look through some of my work and experience! If you like what you see and have a project you need to be coded for, don&apos;t hesitate to send a message to me.</p>
+        <p className={style.text}>Prior to pursuing this career, I was a third-year student at Maritime University. However, due to the closure of schools amidst the COVID-19 outbreak and the political situation in my country, I decided to drop out and explore my true passion. I discovered my aptitude for math and problem-solving during my academic journey, which led me to enroll in an online coding boot camp. Through this program, I gained hands-on experience developing real-world projects from scratch in a remote environment.</p>
+        <p className={style.text}>In addition to my technical skills, I have actively contributed to the open-source community on GitHub for over a year. With more than 2000 contributions to 20 projects, I have gained valuable exposure to collaborative software development. Furthermore, my experience as a general secretary and social welfare officer at the Students&apos; Union of my university honed essential skills such as problem-solving, working under pressure, and effective communication, which I now apply in my role as a software engineer.</p>
+        {/* <p className={style.text}>I can help you build a product, feature, or website. Look through some of my work and experience! If you like what you see and have a project you need to be coded for, don&apos;t hesitate to send a message to me.</p> */}
       </motion.div>
       <motion.div variants={slideIn('right', '', 0, 1)} className={style.img_container}>
         <div className={style.overlay} />
