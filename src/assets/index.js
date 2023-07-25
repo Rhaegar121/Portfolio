5a14f8170bb8fb82999e01c9de4dc864923d704f
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-07-25 23:24:27 +0630

    update social media icon in footer component

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,15 +2,16 @@ import logo from './logo.png';
 
 // socials
 import facebook from './socials/facebook.png';
-import facebook1 from './socials/facebook1.png';
+// import facebook1 from './socials/facebook1.gif';
+import facebook1 from './socials/facebook (1).png';
 import github from './socials/github.png';
-import github1 from './socials/github1.png';
+import github1 from './socials/github (1).png';
 import linkedin from './socials/linkedin.png';
-import linkedin1 from './socials/linkedin1.png';
+import linkedin1 from './socials/linkedin (1).png';
 import twitter from './socials/twitter.png';
-import twitter1 from './socials/twitter1.png';
+import twitter1 from './socials/twitter (1).png';
 import instagram from './socials/instagram.png';
-import instagram1 from './socials/instagram1.png';
+import instagram1 from './socials/instagram (1).png';
 
 // projects
 import tvshowbox from './projects/tvshow.png';
