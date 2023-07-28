47c980a37275585376964704f383b0da6af0bae5
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-07-28 20:40:17 +0630

    add subtitle to service component

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { motion } from 'framer-motion';
 // import { Tilt } from 'react-tilt';
 import { fadeIn, textVariant } from '../utils/motion';
+import SectionWrapper from '../hoc';
 import style from './styles/service.module.css';
 
 const Service = () => (
@@ -10,7 +11,8 @@ const Service = () => (
     <motion.h1 variants={textVariant()} className={style.title}>
       My services
     </motion.h1>
+    <motion.p variants={fadeIn('', '', 0.15, 1)} className={style.subtitle}>What I can do for you -</motion.p>
   </div>
 );
 
-export default Service;
+export default SectionWrapper(Service, 'services', 'my-0');
