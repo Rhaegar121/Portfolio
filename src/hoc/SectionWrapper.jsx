0c6031797c64a6f68f080e2cc41a88151556e258
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-08-19 20:23:13 +0630

    add responsive viewport in sectionwrapper component

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -1,13 +1,16 @@
 import { motion } from 'framer-motion';
+import { useMediaQuery } from 'react-responsive';
 import { staggerContainer } from '../utils/motion';
 
 const SectionWrapper = (Component, idName, margin) => function HOC() {
+  const isLargeScreen = useMediaQuery({ minWidth: 768 });
+
   return (
     <motion.section
       variants={staggerContainer()}
       initial="hidden"
       whileInView="show"
-      viewport={{ once: false, amount: 0.25 }}
+      viewport={{ once: !isLargeScreen, amount: 0.25 }}
       className={`max-w-[90%] mx-auto ${margin}`}
     >
       <span id={idName}>
