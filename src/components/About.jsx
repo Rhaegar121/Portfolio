dd94b9993888de92922ab3dfd522f75b6cd22d64
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-09-03 17:48:39 +0630

    fix eslint errors in about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { motion } from "framer-motion";
-import SectionWrapper from "../hoc";
-import style from "./styles/about.module.css";
-import { textVariant, slideIn } from "../utils/motion";
-import mypicture from "../assets/mypicture.jpg";
+import React from 'react';
+import { motion } from 'framer-motion';
+import SectionWrapper from '../hoc';
+import style from './styles/about.module.css';
+import { textVariant, slideIn } from '../utils/motion';
+import mypicture from '../assets/mypicture.jpg';
 
 const About = () => (
   <>
@@ -11,9 +11,10 @@ const About = () => (
       About Me
     </motion.h1>
     <div className={style.content}>
-      <motion.div variants={slideIn("left", "", 0, 1)} className={style.para}>
+      <motion.div variants={slideIn('left', '', 0, 1)} className={style.para}>
         <p className={style.text}>
-          My name is{" "}
+          My name is
+          {' '}
           <a
             href="https://www.linkedin.com/in/kaungmyatkyaw/"
             target="_blank"
@@ -21,7 +22,8 @@ const About = () => (
             rel="noreferrer"
           >
             Kaung Myat Kyaw (Barry)
-          </a>{" "}
+          </a>
+          {' '}
           and I am a passionate software engineer specializing in delivering
           state-of-the-art software solutions in React and Ruby on Rails. I love
           Math, Coding, Movies, and all kinds of Music.
@@ -39,22 +41,24 @@ const About = () => (
           programming and real-world projects, mastering the skills required to
           thrive in this dynamic field. I completed the boot camp without a
           hitch, showcasing my dedication and determination to succeed. My
-          commitment to the tech community is reflected in my{" "}
+          commitment to the tech community is reflected in my
+          {' '}
           <a
             href="https://github.com/Rhaegar121"
             target="_blank"
             className={style.link}
             rel="noreferrer"
           >
-            {" "}
+            {' '}
             2500+ contributions on GitHub
-          </a>{" "}
+          </a>
+          {' '}
           within a single year. Additionally, my roles in the Students&apos;
           Union honed my problem-solving and communication abilities.
         </p>
       </motion.div>
       <motion.div
-        variants={slideIn("right", "", 0, 1)}
+        variants={slideIn('right', '', 0, 1)}
         className={style.img_container}
       >
         <div className={style.overlay} />
@@ -64,4 +68,4 @@ const About = () => (
   </>
 );
 
-export default SectionWrapper(About, "about", "");
+export default SectionWrapper(About, 'about', '');
