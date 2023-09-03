6ede910b62dd211d574d5cad387cc83c71248e81
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-09-03 22:21:15 +0630

    update contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,28 +1,9 @@
 import React, { useState } from 'react';
-import markerIcon from 'leaflet/dist/images/marker-icon.png';
-import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
-import {
-  MapContainer, TileLayer, Marker, Popup,
-} from 'react-leaflet';
-import L from 'leaflet';
 import { motion } from 'framer-motion';
 import SectionWrapper from '../hoc';
 import { fadeIn, textVariant, slideIn } from '../utils/motion';
+import contact from '../assets/contact.svg';
 import style from './styles/contact.module.css';
-import 'leaflet/dist/leaflet.css';
-
-delete L.Icon.Default.prototype._getIconUrl;
-
-const customIcon = new L.Icon({
-  iconUrl: markerIcon,
-  iconRetinaUrl: markerIcon2x,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  tooltipAnchor: [16, -28],
-  shadowSize: [41, 41],
-  shadowAnchor: [12, 41],
-});
 
 const Contact = () => {
   const [form, setForm] = useState({
@@ -110,26 +91,8 @@ const Contact = () => {
             </button>
           </div>
         </motion.form>
-        <motion.div variants={slideIn('right', '', 0, 1)} className={style.mapContainer}>
-          <MapContainer center={[16.799355, 96.154826]} zoom={13} scrollWheelZoom={false}>
-            <TileLayer
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              attribution="© OpenStreetMap contributors"
-            />
-            <Marker position={[16.799355, 96.154826]} icon={customIcon}>
-              <Popup>
-                Wanna have a virtual coffee?
-                {' '}
-                <br />
-                {' '}
-                Send me a message.
-              </Popup>
-            </Marker>
-            <div className={style.info}>
-              <p>Kaung Myat Kyaw</p>
-              <p>Yangon, Myanmar</p>
-            </div>
-          </MapContainer>
+        <motion.div variants={slideIn('right', '', 0, 1)} className={style.img_container}>
+          <img src={contact} alt="contact" className={style.img} loading="lazy" />
         </motion.div>
       </div>
     </>
